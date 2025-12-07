724df17c0f94dbe831524e9c6d49017b3c41ddb7
steeven lopez <steeven.a.2002@gmail.com>
2025-12-07 01:21:31 +0100

    fix(build): resolve hooks, packages, and profiledef warnings

--- ShadowArch/profiledef.sh
+++ ShadowArch/profiledef.sh
@@ -13,7 +13,6 @@ airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
   ["/root"]="0:0:750"
-  ["/root/install_blackarch.sh"]="0:0:755"
   ["/root/install_script.py"]="0:0:755"
   ["/etc/shadow"]="0:0:400"
   ["/etc/gshadow"]="0:0:400"
